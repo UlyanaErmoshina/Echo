569c78ba8e7825c192b8823232613ea871e1b7c3
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-07-31 11:48:29 +0300

    In commit

---
+++ src/test/java/EchoTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+class EchoTest {
+    @Test
+    void shouldCheckData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("files",equalTo("some data"))
+        ;
+    }
+}
